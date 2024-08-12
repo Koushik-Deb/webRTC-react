dac203b4a7cbcbdb5cd02bd881c79bea5198068a
Koushik Deb <koushik.imon@gmail.com>
2024-08-12 20:05:32 +0600

    update buttons

--- gumPlayground/screenRecorder.js
+++ gumPlayground/screenRecorder.js
@@ -1,6 +1,7 @@
 let mediaRecorder;
 let recordedBlobs;
 const startRecording = async () => {
+  if (!stream) return console.log("No stream to record");
   recordedBlobs = [];
   mediaRecorder = new MediaRecorder(stream);
   mediaRecorder.ondataavailable = (event) => {
@@ -8,16 +9,48 @@ const startRecording = async () => {
     recordedBlobs.push(event.data);
   };
   mediaRecorder.start();
+  changeButtons([
+    "green",
+    "green",
+    "blue",
+    "blue",
+    "green",
+    "blue",
+    "grey",
+    "blue",
+  ]);
 };
 
 const stopRecording = async () => {
+  if (!mediaRecorder) return console.log("No media recorder to stop");
   mediaRecorder.stop();
+  changeButtons([
+    "green",
+    "green",
+    "blue",
+    "blue",
+    "green",
+    "green",
+    "blue",
+    "blue",
+  ]);
 };
 
 const playRecording = async () => {
+  if (!recordedBlobs) return console.log("No recording to play");
   const superBuffer = new Blob(recordedBlobs);
   const recordedVideoEl = document.querySelector("#other-video");
   recordedVideoEl.src = window.URL.createObjectURL(superBuffer);
   recordedVideoEl.controls = true;
   recordedVideoEl.play();
+  changeButtons([
+    "green",
+    "green",
+    "blue",
+    "blue",
+    "green",
+    "green",
+    "green",
+    "blue",
+  ]);
 };
