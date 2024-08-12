1cb5a97bea76d8fcfa38fd2e09a3c951f3c9ba7a
Koushik Deb <koushik.imon@gmail.com>
2024-08-12 11:19:28 +0600

    Few ui updates

--- gumPlayground/scripts.js
+++ gumPlayground/scripts.js
@@ -8,6 +8,16 @@ const getMicAndCamera = async () => {
   try {
     stream = await navigator.mediaDevices.getUserMedia(constraints);
     console.log("Stream:", stream);
+    changeButtons([
+      "green",
+      "blue",
+      "blue",
+      "grey",
+      "grey",
+      "grey",
+      "grey",
+      "grey",
+    ]);
   } catch (error) {
     console.error("Error:", error);
   }
@@ -18,14 +28,35 @@ const showMyFeed = () => {
   videoEl.srcObject = stream; // This will set our MediaStream (stream) to our <video> element
   const tracks = stream.getTracks();
   console.log("Tracks:", tracks);
+  changeButtons([
+    "green",
+    "green",
+    "blue",
+    "blue",
+    "blue",
+    "grey",
+    "grey",
+    "blue",
+  ]);
 };
 
 const stopMyFeed = () => {
+  if (!stream) return console.log("No stream to stop");
   const tracks = stream.getTracks();
   tracks.forEach((track) => track.stop());
   videoEl.srcObject = null;
   stream = null;
   console.log("Stream stopped");
+  changeButtons([
+    "blue",
+    "grey",
+    "grey",
+    "grey",
+    "grey",
+    "grey",
+    "grey",
+    "grey",
+  ]);
 };
 
 document.querySelector("#share").addEventListener("click", getMicAndCamera);
